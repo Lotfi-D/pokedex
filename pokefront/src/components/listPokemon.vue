c96d39db44fc27625e4e5a485ede56c91bc4250c
Lotfi <lotfi.doucanef@epitech.eu>
2020-11-19 00:30:38 +0100

    front

---
+++ pokefront/src/components/listPokemon.vue
@@ -0,0 +1,30 @@
+<template></template>
+
+<script>
+export default {
+    name: "listPokemon",
+
+    data() {
+      return {
+        info: [],
+        page: 1,
+      };
+    },
+
+    methods:{
+
+        getListPokemon(){
+            
+        }
+
+
+
+    }
+
+
+
+
+
+}
+</script>
+
